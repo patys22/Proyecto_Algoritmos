592c520c425f33e3bd9b434b8342d8b4b63cecc4
patys22 <patricia.saputelli@correo.unimet.edu.ve>
2024-08-11 21:55:03 -0400

    carga final

--- Integrantes_csv.py
+++ Integrantes_csv.py
@@ -1,5 +1,17 @@
 import csv
 class Integrantes_csv:
-    def __init__(self, id, nombre):
+    def __init__(self, id, nombre, especie, genero, altura, peso, color_cabello, color_ojos, color_piel, año_nacimiento, planeta, año_muerte, descripcion):
         self.id = id
         self.nombre = nombre
+        self.especie=especie
+        self.genero=genero
+        self.altura=altura
+        self.peso=peso
+        self.color_cabello=color_cabello
+        self.color_ojos=color_ojos
+        self.color_piel=color_piel
+        self.año_nacimiento=año_nacimiento
+        self.planeta=planeta
+        self.año_muerte=año_muerte
+        self.descripcion=descripcion
+        
